644388af7a883349e7fb02dc94de508f14c87580
samuelsingh <samuelsachin@hotmail.co.uk>
2018-03-22 15:22:14 +0000

    added cpu

--- system_info.sh
+++ system_info.sh
@@ -4,108 +4,189 @@
 DATE=`/bin/date +date_%d-%m-%y_time_%H-%M-%S`
 Time(){
 
-###### STARTING PROGRAMMING AND TYPING THE USER INTERFACE ##########
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-echo "<br>"
-echo " Computer Time : `date` <br>"
-echo "<br>"
+#echo "<br>"
+echo " Map Of Medicine Server Details "
+echo " Computer Time : `date` "
+#echo "<br>"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-########### START SCRIPT MAIN PROGRAMMING AND COMMANDS WRITING #####################
+
 }
 GenInfo(){
-## --------------------------------------<General Infromation>-----------------------------------##
-echo "___________________________________________________________________________________<br>"
-echo "**** General Information About This Computer ****"
-echo "___________________________________________________________________________________<br>"
-echo "This Computer Using `uname -m` architecture ;<br>"
-echo "The Linux Kernel Used on this computer `uname -r`<br>"
-echo -e "This Linux Distro. Used On this computer `head -n1 /etc/issue`<br>"
-echo "The Host Name Of this computer is `hostname`<br>"
-echo "The Name Of the user Of this computer is `whoami` <br>"
-echo "The Number Of The Users That Using This Computer `users | wc -w` Users <br>"
-echo "The System Uptime = `uptime | awk '{ gsub(/,/, ""); print $3 }'` (Hrs:Min)<br>"
-echo "The Run level Of Current OS is `runlevel`<br>"
-echo "The Number OF Running Process :`ps ax | wc -l`<br>"
-echo "___________________________________________________________________________________<br>"
+## --------------------------------------<General Information>-----------------------------------##
+echo "___________________________________________________________________________________"
+echo "**** General Information About This instance ****"
+echo "___________________________________________________________________________________"
+echo  "1 - Hostname : `hostname`"
+echo -e "2 - Current OS :\n`lsb_release -a`"
+echo "3 - This Computer Using `uname -m` architecture ;"
+#echo -e "This Linux Distro. Used On this computer `head -n1 /etc/issue`"
+#echo "The Host Name Of this computer is `hostname`"
+#echo "The Name Of the user Of this computer is `whoami` "
+#echo "The Number Of The Users That Using This Computer `users | wc -w` Users "
+echo -e "4 - The System Uptime = `uptime | awk '{ gsub(/,/, ""); print $3 }'` (Hrs:Min)"
+echo "5 - The Run level Of Current OS is `runlevel`"
+#echo "The Number OF Running Process :`ps ax | wc -l`"
+echo -e "6 -PSTREE:\n`pstree`"
+echo -e "7 -System v interprocess:\n`ipcs -a`"
+echo -e "8 -System Load Avg:\n`w`"
+echo -e "9 -Environment variables:\n`env`"
+echo -e "10 -All users:\n`cat /etc/passwd`"
+echo -e "11 -All groups:\n`cat /etc/group`"
+echo -e "11 -All files under /var/log:\n`la -ltr /var/log`"
+echo "___________________________________________________________________________________"
 ## --------------------------------------<General Infromation/end>-------------------------------##
 }
 
-CPUInfo(){ ## --------------------------------------<CPU Infromation>---------------------------------------##
-echo "___________________________________________________________________________________<br>"
-echo "****The CPU Infromation****<br>"
-echo "___________________________________________________________________________________<br>"
-echo "You Have `grep -c 'processor' /proc/cpuinfo` CPU<br>"
-echo "Your CPU model name is `awk -F':' '/^model name/ { print $2 }' /proc/cpuinfo`<br>"
-echo "Your CPU vendor`awk -F':' '/^vendor_id/ { print $2 }' /proc/cpuinfo`<br>"
-echo "Your CPU Speed`awk -F':' '/^cpu MHz/ { print $2 }' /proc/cpuinfo`<br>"
-echo "Your CPU Cache Size`awk -F':' '/^cache size/ { print $2 }' /proc/cpuinfo`<br>"
-echo "___________________________________________________________________________________<br>"
+KernelInfo(){ ## --------------------------------------<Kernel Information---------------------------------------##
+echo "___________________________________________________________________________________"
+echo "****Kernel Information****"
+echo "___________________________________________________________________________________"
+echo  "1 - Kernel version: `uname -r`"
+##ss##echo -e "2 - Sysctl -A:\n`sysctl -A`"
+echo "___________________________________________________________________________________"
 
 ## --------------------------------------<CPU Infomation/end>-----------------------------------##
 
 }
 
+
 MemInfo(){
 ## --------------------------------------<memory Information>------------------------------------##
-echo "___________________________________________________________________________________<br>"
-echo " ****The Memory Information****<br>"
-echo "___________________________________________________________________________________<br>"
-echo "`cat /proc/meminfo`<br>"
-echo "___________________________________________________________________________________<br>"
-echo "`free -m`<br>"
-echo "___________________________________________________________________________________<br>"
+echo "___________________________________________________________________________________"
+echo " ****Memory Information****"
+echo "___________________________________________________________________________________"
+echo -e "1 - MemInfo:\n`cat /proc/meminfo`"
+echo "___________________________________________________________________________________"
+
+echo -e "2 - Free:\n `free -m`"
+echo "___________________________________________________________________________________"
 
 ## --------------------------------------<memory Infromation/end>--------------------------------##
 }
 
-FileSInfo(){
+CPUInfo(){ ## --------------------------------------<CPU Infromation>---------------------------------------##
+echo "___________________________________________________________________________________"
+echo "****CPU Information****"
+echo "___________________________________________________________________________________"
+echo "You Have `grep -c 'processor' /proc/cpuinfo` CPU"
+echo "Your CPU model name is `awk -F':' '/^model name/ { print $2 }' /proc/cpuinfo`"
+echo "Your CPU vendor`awk -F':' '/^vendor_id/ { print $2 }' /proc/cpuinfo`"
+echo "Your CPU Speed`awk -F':' '/^cpu MHz/ { print $2 }' /proc/cpuinfo`"
+echo "Your CPU Cache Size`awk -F':' '/^cache size/ { print $2 }' /proc/cpuinfo`"
+echo "___________________________________________________________________________________"
 
-## --------------------------------------<File Systems Infromation>-------------------------------##
-echo "___________________________________________________________________________________<br>"
-echo "*****File Systems Infromation******<br>"
-echo "___________________________________________________________________________________<br>"
-echo "`df -h`<br>"
-echo "___________________________________________________________________________________<br>"
+## --------------------------------------<CPU Infomation/end>-----------------------------------##
 
-## --------------------------------------<File Systems Infromation/end>------------------------------##
 }
+
+BootInfo(){ ## --------------------------------------<Boot Infromation>---------------------------------------##
+echo "___________________________________________________________________________________"
+echo "****Boot Information****"
+echo "___________________________________________________________________________________"
+echo "`cat /var/log/boot.log`"
+echo "___________________________________________________________________________________"
+
+}
+
+
+
+DiskInfo(){ ## --------------------------------------<Disk Infromation>---------------------------------------##
+echo "___________________________________________________________________________________"
+echo "****Disk Infromation****"
+echo "___________________________________________________________________________________"
+echo -e "1 - Disks Information:\n`sudo fdisk -l`"
+echo "___________________________________________________________________________________"
+echo -e "2 - Partition table details:\n`sudo parted --list`"
+echo "___________________________________________________________________________________"
+echo -e "3 - List of block devices:\n`lsblk`"
+echo "___________________________________________________________________________________"
+echo -e "4 - List of block device attributes:\n`blkid`"
+echo "___________________________________________________________________________________"
+echo -e "5 - File system disk space :\n`df -h`"
+echo "___________________________________________________________________________________"
+echo -e "6 - Inode Information:\n`df -i`"
+echo "___________________________________________________________________________________"
+echo -e "7 - DMI Setup Information:\n`dmidecode`"
+}
+
+
+
+
 
 PCIInfo(){
 ## --------------------------------------<Pci Devices Information script>-------------------------##
-echo "___________________________________________________________________________________<br>"
-echo "******PCI devices On MOtherboard information {detailed}******<br>"
-echo "___________________________________________________________________________________<br>"
-echo "`lspci -tv`<br>"
-echo "___________________________________________________________________________________<br>"
+echo "___________________________________________________________________________________"
+echo "******PCI devices On MOtherboard information {detailed}******"
+echo "___________________________________________________________________________________"
+echo "`lspci -tv`"
+echo "___________________________________________________________________________________"
 ## --------------------------------------<Pci Devices Information script/end>----------------------##
 
 }
 
 NetInfo(){
 ## --------------------------------------<Netwrok Infromation>-------------------------------------##
-echo "___________________________________________________________________________________<br>"
-echo "********Network Information********<br>"
-echo "___________________________________________________________________________________<br>"
-echo "`/sbin/ifconfig`<br>"
-echo "___________________________________________________________________________________<br>"
+echo "___________________________________________________________________________________"
+echo "********Network Information********"
+echo "___________________________________________________________________________________"
+
+echo "___________________________________________________________________________________"
+cho -e "1 - Network Connections:\n`netstat -ntpl`"
+echo "___________________________________________________________________________________"
+echo -e "2 - Routing Table details:\n`netstat -nr`"
+echo "___________________________________________________________________________________"
+echo -e "3 - Interface Stats:\n`ifconfig`"
+echo "___________________________________________________________________________________"
+echo -e "4 - Multicast groups:\n`netstat -g`"
+echo "___________________________________________________________________________________"
+echo -e "5 - Iptables/Firewall status :\n`ufw status`"
+echo "___________________________________________________________________________________"
+echo -e "6 - NTP configuration:\n`cat /etc/ntp.conf`"
+echo "___________________________________________________________________________________"
+echo -e "7 - DNS configuration:\n`cat /etc/resolv.conf`"
 ## --------------------------------------<network Infromation/end>--------------------------------##
+
+}
+
+
+
+ApacheInfo(){
+## --------------------------------------<Apache  Infromation>-------------------------------------##
+echo "___________________________________________________________________________________"
+echo "****Apache Infromation****"
+SERVICE='apache2'
+ 
+if ps ax | grep -v grep | grep $SERVICE > /dev/null
+then
+    echo -e "$SERVICE service running\n"
+    echo -e "Installed Package\n:`sudo dpkg -l | grep -i apache`"
+else
+    #echo "$SERVICE is not running"
+    echo "This is not a web server"
+    #echo "$SERVICE is not running!" | mail -s "$SERVICE down" root
+fi
 
 }
 
 ## --------------------------------------<script ending>-----------------------------------------##
 
 Run(){
-echo "<html><body>"
+#echo "<html><body>"
 Time
 GenInfo
-CPUInfo
+KernelInfo
 MemInfo
-FileSInfo
-PCIInfo
+CPUInfo
+BootInfo
+DiskInfo
+ApacheInfo
+#FileSInfo
+#PCIInfo
 NetInfo
-echo "</body></html>"
+#echo "</body></html>"
 }
-log=Sysinfo_$DATE
-Run | tee $log.txt
-mv $log.txt $log.html
+log=`hostname | cut -d"-" -f1`_$DATE
+Run | tee $log.sysinfo
+#mv $log.txt $log.html
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
